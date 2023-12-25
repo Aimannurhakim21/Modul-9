b1b3a74c46079f44e7a42264a11f296e49170bda
LAPTOP-KCETHLSE\LENOVO <aiman.nurhakim117@gmail.com>
2023-12-25 17:19:56 +0700

    Program tugas Group.java setelah dilakukan Refactoring

--- Group.java
+++ Group.java
@@ -1,24 +1,35 @@
 public class Group {
-    public List<User> getUsers() {
+    public List getUsersSortedByMostRecentlyRegistered() {
         List<User> users = new ArrayList<>();
-        String persistencePath = persistencePath();
-        File persistenceDirectory = new File(persistencePath);
 
-        if (!persistenceDirectory.exists()) {
+        if (!userDirectoryExists()) {
             return users;
         }
 
-        File[] files = persistenceDirectory.listFiles();
+        addFoundUsersTo(users);
+        sortByMostRecentlyRegistered(users);
+
+        return users;
+    }
+
+    private boolean userDirectoryExists() {
+        String persistencePath = getPersistencePath();
+        File persistenceDirectory = new File(persistencePath);
+        return persistenceDirectory.exists();
+    }
+
+    private void addFoundUsersTo(List<User> users) {
+        File[] files = new File(getPersistencePath()).listFiles();
 
         for (File file : files) {
             if (file.isDirectory()) {
                 users.add(new User(file.getName(), this));
             }
         }
+    }
 
+    private void sortByMostRecentlyRegistered(List<User> users) {
         Collections.sort(users, new User.UserComparatorByDescendingRegistration());
-
-        return users;
     }
 }
 
