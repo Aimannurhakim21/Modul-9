5f54a29b2a2e841557ae810689f396821f60f086
LAPTOP-KCETHLSE\LENOVO <aiman.nurhakim117@gmail.com>
2023-12-25 16:54:11 +0700

    Program ConditionalExpression.java setelah di refactoring

--- ConditionalExpression.java
+++ ConditionalExpression.java
@@ -1,6 +1,6 @@
 public class ConditionalExpression {
     if((score<=700)&&((income<40000)||(income>100000)||!authorized||(score<=500))&&(income<=100000))
- reject()
-else
- accept();
+    reject()
+    else
+     accept();
 }
